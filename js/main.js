d8c81fe2fcc22a1838edce993b0a000130119e4a
Ryutsashi <ryutsashi@gmail.com>
2025-04-18 19:30:48 +0200

    a crude merge of CameronGott's v0.4 branch into mine.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var gameData = {
     taskData: {},
     itemData: {},
     townData: {},
+
     rawTownIncome: 0,
     coins: 0,
     days: 365 * 14,
@@ -16,6 +17,10 @@ var gameData = {
     currentSkill: null,
     currentProperty: null,
     currentMisc: null,
+
+    totalCitizens: 0,
+    assignedCitizens: 0,
+    idleCitizens: 0,
 }
 
 //tempData is used during initial game setup. 
@@ -519,8 +524,15 @@ function applyMultipliers(value, multipliers) {
     var finalMultiplier = 1;
     multipliers.forEach(function(multiplierFunction) {
         //wtf is multiplier function? It's called like a function, but we have no function definition ANYWHERE. Mrrrrr...
-        var multiplier = multiplierFunction();
-        finalMultiplier *= multiplier;
+      if (multiplierFunction !== null) {
+        try {
+          var multiplier = multiplierFunction();
+          finalMultiplier *= multiplier;
+        } catch (e) {
+          console.log(multiplierFunction);
+          console.trace();
+        }
+      }
     });
     var finalValue = Math.round(value * finalMultiplier);
     return finalValue;
@@ -592,7 +604,8 @@ function setTab(element, selectedTab) {
 }
 
 function setPause() {
-    gameData.paused = !gameData.paused
+  gameData.paused = !gameData.paused
+  updateUI();
 }
 
 function setTimeWarping() {
@@ -1389,6 +1402,7 @@ function updateUI() {
 }
 
 function update() {
+  if (!gameData.paused) {
     increaseDays();
     autoPromote();
     autoLearn();
@@ -1396,6 +1410,7 @@ function update() {
     doCurrentTask(gameData.currentSkill);
     applyExpenses();
     updateUI();
+  }
 }
 
 function resetGameData() {
@@ -1419,6 +1434,40 @@ function importGameData() {
 function exportGameData() {
     var importExportBox = document.getElementById("importExportBox");
     importExportBox.value = window.btoa(JSON.stringify(gameData));
+}
+
+function uploadGameData() {
+    var input = document.getElementById("uploadSaveInput");
+    if (input.files.length === 0) {
+        alert("Please select a file to upload");
+        return;
+    }
+    var file = input.files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = function () {
+        var data = JSON.parse(window.atob(reader.result));
+        gameData = data;
+        saveGameData();
+        location.reload();
+    };
+}
+
+function downloadGameData() {
+    var filename = "progressKnightReborn.sav";
+    var data = window.btoa(JSON.stringify(gameData));
+    var file = new Blob([data], { type: "text/plain" });
+
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else {
+        let saveFile = document.createElement("a");
+        saveFile.download = filename;
+        saveFile.href = URL.createObjectURL(file);
+        saveFile.click();
+        URL.revokeObjectURL(saveFile.href);
+    }
 }
 
 function registerEventListeners() {
@@ -1614,5 +1663,5 @@ setTab(jobTabButton, "jobs");
 
 update();
 setInterval(update, 1000 / updateSpeed);
-setInterval(saveGameData, 3000);
+setInterval(saveGameData, 6000);
 setInterval(setSkillWithLowestMaxXp, 1000);
